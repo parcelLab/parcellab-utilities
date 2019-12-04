parcelLab/parcellab-utilities

commit e6400f046e32be41bd2c526c8843e5979e3a073d

turn _extras into msgLong if it has no msgLong key

src/logger.js
@@ -188,12 +188,8 @@ function logToConsole(type, sender, msgShort, _extras) {
 
   // sender = sender.toUpperCase()
 
-  let msgLong = ''
-  if (extras.msgLong) {
-    msgLong = extras.msgLong
-    if (_.isObject(msgLong)) msgLong = JSON.stringify(msgLong)
-    delete extras.msgLong
-  }
+  let msgLong = extras.msgLong || _extras
+  if (_.isObject(msgLong)) msgLong = JSON.stringify(msgLong)
 
   // in production mode send the message to graylog as well.
   if (!logger.settings.developer_mode) {

test/logger.js
@@ -99,7 +99,7 @@ export default function () {
         Logger.settings.port = null
         assert.strictEqual((() => {
           Logger.initGraylog()
-          logToConsole('error', 'graylog', 'this should raise an error')
+          logToConsole('error', 'graylog', 'this should raise an error', { what: 'tehfck' })
         })(), undefined)
         Logger.settings.level = 'DEBUG' // decent logging for following tests
         Logger.settings.verboseLocal = false // but dont overdo

commit 497445f542430a5a0431aeeaf555161f835ca7ce

fix local logging bug introduced in e6400f046e32be41bd2c526c8843e5979e3a073d

src/logger.js
@@ -147,7 +147,8 @@ function logLocal(type, sender, msgShort, msgLong, extras) {
   let msg = (logger.settings.color ? colors.Reset : '') +
             (logger.settings.timestampLocal ? colorize('timestamp', (new Date()).toJSON()) + ' ' : '') + 
             colorize('level', type) + '<' + colorize('sender', sender) + '>: '
-  msg += msgLong ? msgLong : msgShort
+  // msg += msgLong ? msgLong : msgShort // nope, msgLong will usually be == extras 
+  msg += msgShort;
   //if (['ERROR', 'WARN'].indexOf(type) !== -1) console.error(msg)
   //else 
   console.log(msg)