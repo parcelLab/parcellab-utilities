9d930e2256ed634384384e6e956cd20fe3b43d71
Carlos <835129+cvscarlos@users.noreply.github.com>
2024-08-01 11:54:58 +0200

    chore: accept the `user _id` field in camelCase and snake_case formats (#26)

--- src/logger.js
+++ src/logger.js
@@ -196,6 +196,7 @@ function logToConsole(type, sender, msgShort, _extras) {
       type: type,
     }
     if (extras.user_id) smallExtras.user_id = Number(extras.user_id) || 0
+    if (extras.userId && !smallExtras.user_id) smallExtras.user_id = Number(extras.userId) || 0
     if (extras.filename) smallExtras.filename = extras.filename
 
     // limit size of msgLong to avoid excessive storage consumtion and failures
